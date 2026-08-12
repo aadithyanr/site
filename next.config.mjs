5c1e5355828396116d4c98fdcfcf271e41286f6a
aadithyan rajesh <nair.aadithyan999@gmail.com>
2026-08-13 03:04:18 +0530

    set up the site foundation

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["api.microlink.io", "i.pinimg.com", "i.postimg.cc"],
+  },
+};
+
+export default nextConfig;
